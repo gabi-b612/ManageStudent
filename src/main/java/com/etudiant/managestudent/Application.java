4e8cff18e2327695a4aa0e10968efd8a9cf35299
gabi-b612 <20km095@esisalama.org>
2024-07-06 10:12:36 +0200

    [feat] : delete the barre title

--- src/main/java/com/etudiant/managestudent/Application.java
+++ src/main/java/com/etudiant/managestudent/Application.java
@@ -1,17 +1,38 @@
 package com.etudiant.managestudent;
 
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.IOException;
+import java.util.Objects;
+import java.util.concurrent.atomic.AtomicReference;
 
 public class Application extends javafx.application.Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("save-student.fxml")));
+
+        AtomicReference<Double> xOffset = new AtomicReference<>((double) 0);
+        AtomicReference<Double> yOffset = new AtomicReference<>((double) 0);
+        root.setOnMousePressed(event -> {
+            xOffset.set(event.getSceneX());
+            yOffset.set(event.getSceneY());
+        });
+
+        root.setOnMouseDragged(event -> {
+            stage.setX(event.getScreenX() - xOffset.get());
+            stage.setY(event.getScreenY() - yOffset.get());
+        });
+
+        Scene scene = new Scene(root, 1020, 600);
+        stage.setTitle("Manage Student");
+
+        stage.initStyle(StageStyle.UNDECORATED);
+
         stage.setScene(scene);
         stage.show();
     }
